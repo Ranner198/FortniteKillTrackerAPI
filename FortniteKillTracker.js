1bb6b07aa642e7fb6c3c92ebd41b582dc6277756
Ran Crump <rancrump18@gmail.com>
2018-04-10 18:36:23 -0500

    V0.02
    Lots of logic, added a time table to compare current day and about to add daily events based off the comparisons. Actually sorts correctly using JSON.

--- FortniteKillTracker.js
+++ FortniteKillTracker.js
@@ -11,10 +11,52 @@ request({
  	headers: {"TRN-Api-Key": "2d4d442a-937a-4e08-987e-205536a23cde"},
 
 }, function(err, res, body) {
-  	var totalWins = JSON.parse(body);
+  	var data = JSON.parse(body);
 
-  	console.log(totalWins.lifeTimeStats);
- 	
-  	
- 	
+  	/*
+  	var totalWins = data.lifeTimeStats[8].value;
+	var totalKills = data.lifeTimeStats[10].value;
+
+  	var Output = "My Total Wins are: " + totalWins.toString() + ", My Total Kills are: "
+  	 + totalKills.toString();	
+  	 */
+  	 var array = [];
+  	 var placeholder = "";
+  	 var fixedInfo = "";
+
+  	 var d = new Date();
+  	 var year = d.getFullYear();
+  	 var month = '' + (d.getMonth() + 1);
+  	 var day = '' + d.getDate();
+
+  	if (month.length < 2)
+  		month = '0' + month;
+    if (day.length < 2)
+    	day = '0' + day;
+
+  	 var today = year + "-" + month + "-" + day;
+
+  	 for (var i = 0; i < data.recentMatches.length; i++) {
+  	 	placeholder = "";
+  	 	for (var j = 0; j < 10; j++) {
+  	 		placeholder += (data.recentMatches[i].dateCollected[j]).toString();
+
+  	 	}	
+  	 	array[i] = placeholder;
+  	 }
+  	 for (var i = 0; i < array.length; i++) {
+  	 	fixedInfo = array.join(",");
+  	 }
+  	 console.log(fixedInfo);
+  	 console.log("\n" + today);
+
+  	 var times = 0;
+
+  	 for (var x = 0; x < array.length; x++) {
+  	 	if (array[x] == today)
+  	 		times++;
+  	 }
+	console.log(times);
+
+	console.log(array[3]);
 });
